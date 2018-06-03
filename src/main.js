bc2e73f0d5cf70df40e9061d4322cbb9f6e47d13
David AZOULAY <dazoulay@simplicite.fr>
2018-06-03 22:34:34 +0200

    Updated

--- src/main.js
+++ src/main.js
@@ -2,7 +2,25 @@ import Vue from 'vue'
 import Simplicite from 'simplicite'
 import App from './App.vue'
 
+let app = Simplicite.session({
+	url: 'https://demo.dev.simplicite.io',
+	username: 'website',
+	password: 'simplicite',
+	debug: true
+});
+
+app.login().then(function(params) {
+	console.log('Logged in as ' + params.username);
+	return app.getGrant(); // next promise
+}, function(reason) {
+	app = null;
+	console.log('Login failed (status: ' + reason.status + ', message: ' + reason.message + ')');
+}).then(function(grant) {
+	if (!app) return;
+	console.log('Hello ' + grant.getLogin() + '!');
+});
+
 new Vue({
-  el: '#app',
+  el: '#main',
   render: h => h(App)
 })
