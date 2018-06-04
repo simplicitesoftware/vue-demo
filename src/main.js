447f9b6b873d971e23cdb5f1594c5ff3dc1ab272
David AZOULAY <dazoulay@simplicite.fr>
2018-06-04 13:06:13 +0200

    Updated

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Simplicite from 'simplicite'
 
+var debug = false;
+
 var vm = new Vue({
 	el: '#app',
 	data: {
+		message: '',
 		grant: {},
 		products: []
 	},
@@ -13,20 +16,22 @@ var app = Simplicite.session({
 	url: 'https://demo.dev.simplicite.io',
 	username: 'website',
 	password: 'simplicite',
-	debug: true
+	debug: debug
 }), prd;
 
 app.login().then(function(params) {
 	return app.getGrant(); // next promise
 }, function(reason) {
 	app = null;
-	console.log('Login failed (status: ' + reason.status + ', message: ' + reason.message + ')');
+	vm.message = 'Login failed (status: ' + reason.status + ', message: ' + reason.message + ')';
 }).then(function(grant) {
 	if (!app) return;
+	if (debug) console.log(grant);
 	vm.grant = grant;
 	prd = app.getBusinessObject('DemoProduct');
-	return prd.search(null, { inlineThumbs: true }); // next promise
+	return prd.search(); // next promise
 }).then(function(list) {
 	if (!app) return;
+	if (debug) console.log(list);
 	vm.products = list;
 });
