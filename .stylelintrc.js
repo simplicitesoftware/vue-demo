cd3c075467b1ca757cb3061d7a2411fc1d70b27d
David AZOULAY <dazoulay@simplicite.fr>
2023-01-12 00:28:43 +0100

    Stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'extends': 'stylelint-config-recommended-vue',
+  'rules': {
+    'indentation': 2,
+    'declaration-empty-line-before': 'never',
+    'number-leading-zero': 'never'
+  }
+};
