c937f3d5ace8f747ba78c8134648bac81be39d46
David AZOULAY <dazoulay@simplicite.fr>
2023-01-12 00:06:19 +0100

    ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  'env': {
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+  ],
+  'rules': {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'no-multiple-empty-lines': ['error', { 'max': 1 }],
+    'indent': ['error', 2],
+    'vue/html-indent': ['error', 2],
+    'vue/max-attributes-per-line': 'off',
+    'vue/first-attribute-linebreak': 'off',
+    'vue/html-closing-bracket-spacing': 'off',
+    'vue/singleline-html-element-content-newline': 'off'
+  },
+  'ignorePatterns': [
+    'dist'
+  ]
+};
