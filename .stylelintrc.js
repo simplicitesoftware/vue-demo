a0814e86dfa0a03d2f7f0988fbfa9ad5a1549e13
David AZOULAY <dazoulay@simplicite.fr>
2024-12-11 14:02:29 +0100

    ESLint config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   'extends': 'stylelint-config-recommended-vue',
   'rules': {
-    'rule-empty-line-before': 'always',
+    'rule-empty-line-before': 'never',
     'declaration-empty-line-before': 'never',
     'declaration-block-no-redundant-longhand-properties': null,
-    'color-hex-length': 'long'
+    'color-hex-length': 'short',
   }
 };
---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import eslintPluginVue from "eslint-plugin-vue"
+import globals from "globals";
+
+export default [ ...eslintPluginVue.configs["flat/recommended"], {
+    languageOptions: {
+        globals: { ...globals.browser }
+    },
+    rules: {
+        "indent": ["error", 2],
+        "quotes": ["error", "single"],
+        "semi": ["error", "always"],
+        "no-multiple-empty-lines": ["error", { max: 1 }],
+        "no-multi-spaces": "error",
+        "no-trailing-spaces": ["error", { skipBlankLines: false }],
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        "vue/html-indent": ["error", 2],
+        "vue/max-attributes-per-line": "error",
+        "vue/first-attribute-linebreak": "error",
+        "vue/html-closing-bracket-spacing": ["error", { startTag: "never", endTag: "never", selfClosingTag: "never" }],
+        "vue/singleline-html-element-content-newline": "error"
+    }
+}];
