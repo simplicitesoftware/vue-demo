b41052e1c3069f6cfb5bfeb597edd012a7015be4
David AZOULAY <dazoulay@simplicite.fr>
2026-04-02 23:21:15 +0200

    Bump dev deps

--- src/components/DemoAddProduct.vue
+++ src/components/DemoAddProduct.vue
@@ -12,17 +12,13 @@ export default {
   methods: {
     async add() {
       const vm = this;
-      // console.log(vm.ref, vm.name);
       const prd = vm.$simplicite.getBusinessObject('DemoProduct');
       let item = await prd.getForCreate();
-      // console.log(item);
       item.demoPrdSupId = 1;
       item.demoPrdType = 'OTHER';
       item.demoPrdReference = vm.ref;
       item.demoPrdName = vm.name;
-      // console.log(prd.item);
-      item = await prd.save(item);
-      // console.log(item);
+      await prd.save(item);
     }
   }
 };
